6a5981473cec3413eb6db38a9e2d94495e68426b
senbo1 <harshbhadu86@gmail.com>
2025-02-03 16:39:11 +0530

    added some packages and setup dm

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -6,6 +6,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
+app.post('/signup', async (req: Request, res: Response) => {});
+
+app.post('/signin', (req: Request, res: Response) => {});
+
+app.post('/room', (req: Request, res: Response) => {});
+
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
